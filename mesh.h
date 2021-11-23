0a8b5f62c186fa61246bda3c46b899d5b2232c35
Aaron C Gaudette <acg@aaronsee.media>
2021-11-23 14:29:01 -0500

    Rewrite mesh header

--- mesh.h
+++ mesh.h
@@ -1,211 +1,252 @@
+enum mesh_flags {
+	  MESH_NONE = 0x0
+	, MESH_TMP  = 0x1
+};
+
+struct verts {
+	u32  n;
+	fff *v;
+};
+
+static struct verts verts_new(const u32 n, int tmp)
+{
+	struct verts result = { .n = n };
+	const u32 size = result.n * sizeof(fff);
+	result.v = tmp ? bump(size) : alloc(size, ALLOC_SYS);
+	assert(result.v);
+	return result;
+}
+
+static mesh verts_to_mesh(const struct verts verts)
+{
+	assert(!(verts.n % 3));
+	mesh result = mesh_new(verts.n / 3, verts.v);
+	return result;
+}
+
 typedef struct {
-	ff   ext;
-	fff  pos;
-	ffff rot;
+	v3 pts[4];
+	enum mesh_flags flags;
 } quad;
 
-static u32 quad_sprint(quad quad, mesh *out, u32 off)
+static struct verts quad_verts(const quad quad)
 {
-	const u32 n = 3 * 2;
+	const u32 n = 6;
+	struct verts result = verts_new(n, quad.flags & MESH_TMP);
 
-	fff pts[] = {
-		{ 0, 0, 0 },
-		{ 0, 0, 1 },
-		{ 1, 0, 1 }, // Tri
-		{ 1, 0, 1 },
-		{ 1, 0, 0 },
-		{ 0, 0, 0 } // Tri
+	const v3 pts[] = {
+		quad.pts[0],
+		quad.pts[1],
+		quad.pts[2],
+		quad.pts[2],
+		quad.pts[3],
+		quad.pts[0],
 	};
 
-	for (u32 i = 0; i < n; ++i) {
-		pts[i] = schurfff(
-			pts[i],
-			(fff) { quad.ext.x, 0.f, quad.ext.y }
-		);
+	memcpy(result.v, pts, n * sizeof(fff));
+	return result;
+}
 
-		pts[i] = qt_app(quad.rot, pts[i]);
-		pts[i] = addfff(pts[i], quad.pos);
+static mesh quad_to_mesh(const quad quad)
+{
+	return verts_to_mesh(quad_verts(quad));
+}
+
+typedef struct {
+	ff anc;
+	ff ext;
+	v4 rot;
+	enum mesh_flags flags;
+} bill;
+
+static struct verts bill_verts(const bill bill)
+{
+	const ff anchor = mulff(addff(bill.anc, V2_ONE), .5f);
+	const float xl = lerpf(0.f, 1.f, -anchor.x);
+	const float xr = lerpf(1.f, 0.f,  anchor.x);
+	const float yl = lerpf(0.f, 1.f, -anchor.y);
+	const float yr = lerpf(1.f, 0.f,  anchor.y);
+
+	quad quad = {
+		.pts = {
+			{ xl, yl, 0.f },
+			{ xl, yr, 0.f },
+			{ xr, yr, 0.f },
+			{ xr, yl, 0.f },
+		},
+		.flags = bill.flags,
+	};
+
+	for (u32 i = 0; i < 4; ++i) {
+		quad.pts[i] = schurfff(quad.pts[i], v3_padxy(bill.ext, 0.f));
+		quad.pts[i] = qt_app(bill.rot, quad.pts[i]);
 	}
 
-	memcpy(out->pts + off * 3, &pts, n * sizeof(fff));
-	return 2;
+	return quad_verts(quad);
 }
 
-typedef struct {
-	fff ext;
-	fff pos;
-	fff anc;
-} cube;
-
-static mesh cube_conv(cube cube)
+static mesh bill_to_mesh(const bill bill)
 {
-	mesh result = mesh_new(6 * 2);
-	u32 off = 0;
-	fff anchor = schurfff(cube.ext, v3_neg(cube.anc));
-
-	off += quad_sprint( // Bot
-		(quad) {
-			.ext = { -cube.ext.x, cube.ext.z },
-			.pos = addfff(cube.pos, anchor),
-			.rot = qt_vecs(V3_FWD, V3_DN),
-		},
-		&result,
-		off
-	);
-
-	off += quad_sprint( // Top
-		(quad) {
-			.ext = { cube.ext.x, cube.ext.z },
-			.pos = addfff(
-				cube.pos,
-				addfff(
-					(fff) { 0, cube.ext.y, 0 },
-					anchor
-				)
-			),
-			.rot = qt_vecs(V3_FWD, V3_UP),
-		},
-		&result,
-		off
-	);
-
-	off += quad_sprint( // Back
-		(quad) {
-			.ext = { cube.ext.x, cube.ext.y },
-			.pos = addfff(cube.pos, anchor),
-			.rot = qt_vecs(V3_UP, V3_BCK),
-		},
-		&result,
-		off
-	);
-
-	off += quad_sprint( // Front
-		(quad) {
-			.ext = { -cube.ext.x, cube.ext.y },
-			.pos = addfff(
-				cube.pos,
-				addfff(
-					(fff) { 0, 0, cube.ext.z },
-					anchor
-				)
-			),
-			.rot = qt_vecs(V3_UP, V3_FWD),
-		},
-		&result,
-		off
-	);
-
-	off += quad_sprint( // Left
-		(quad) {
-			.ext = { cube.ext.y, cube.ext.z },
-			.pos = addfff(cube.pos, anchor),
-			.rot = qt_vecs(V3_FWD, V3_LFT),
-		},
-		&result,
-		off
-	);
-
-	off += quad_sprint( // Right
-		(quad) {
-			.ext = { -cube.ext.y, cube.ext.z },
-			.pos = addfff(
-				cube.pos,
-				addfff(
-					(fff) { cube.ext.x, 0, 0 },
-					anchor
-				)
-			),
-			.rot = qt_vecs(V3_FWD, V3_RT),
-		},
-		&result,
-		off
-	);
-
-	return result;
-}
-
-typedef struct {
-} icosph;
-
-// schneide.blog/2016/07/15/generating-an-icosphere-in-c++
-static mesh icosph_conv(icosph ico)
-{
-	mesh result = mesh_new(20);
-
-	const float X = .525731112119133606f;
-	const float Z = .850650808352039932f;
-	const float N = 0.f;
-
-	static const v3 verts[] = {
-		{ -X, N, Z }, {  X, N,  Z }, { -X,  N, -Z }, {  X,  N, -Z },
-		{  N, Z, X }, {  N, Z, -X }, {  N, -Z,  X }, {  N, -Z, -X },
-		{  Z, X, N }, { -Z, X,  N }, {  Z, -X,  N }, { -Z, -X,  N },
-	};
-
-	const v3 all[] = {
-		verts[0],  verts[4],  verts[1],
-		verts[0],  verts[9],  verts[4],
-		verts[9],  verts[5],  verts[4],
-		verts[4],  verts[5],  verts[8],
-		verts[4],  verts[8],  verts[1],
-		verts[8],  verts[10], verts[1],
-		verts[8],  verts[3],  verts[10],
-		verts[5],  verts[3],  verts[8],
-		verts[5],  verts[2],  verts[3],
-		verts[2],  verts[7],  verts[3],
-		verts[7],  verts[10], verts[3],
-		verts[7],  verts[6],  verts[10],
-		verts[7],  verts[11], verts[6],
-		verts[11], verts[0],  verts[6],
-		verts[0],  verts[1],  verts[6],
-		verts[6],  verts[1],  verts[10],
-		verts[9],  verts[0],  verts[11],
-		verts[9],  verts[11], verts[2],
-		verts[9],  verts[2],  verts[5],
-		verts[7],  verts[2],  verts[11],
-	};
-
-	memcpy(result.pts, &all, 20 * 3 * sizeof(v3));
-	return result;
+	return verts_to_mesh(bill_verts(bill));
 }
 
 typedef struct {
 	v3 ext;
-} cubef;
+	v3 anc;
+	enum mesh_flags flags;
+} cuboid;
 
-static mesh cubef_conv(cubef cube)
+static struct verts cuboid_verts(const cuboid cuboid)
 {
-	mesh result = mesh_new(6 * 2);
+	const u32 n = 6 * 2 * 3;
+	struct verts result = verts_new(n, cuboid.flags & MESH_TMP);
+	const v3 anchor = v3_mul(v3_add(cuboid.anc, V3_ONE), .5f);
 
-	const float xl = -cube.ext.x * .5f;
-	const float xr =  cube.ext.x * .5f;
+	const float xl = lerpf(0.f, cuboid.ext.x, -anchor.x);
+	const float xr = lerpf(cuboid.ext.x, 0.f,  anchor.x);
 
-	const float yl = -cube.ext.y * .5f;
-	const float yr =  cube.ext.y * .5f;
+	const float yl = lerpf(0.f, cuboid.ext.y, -anchor.y);
+	const float yr = lerpf(cuboid.ext.y, 0.f,  anchor.y);
 
-	const float zl = -cube.ext.z * .5f;
-	const float zr =  cube.ext.z * .5f;
+	const float zl = lerpf(0.f, cuboid.ext.z, -anchor.z);
+	const float zr = lerpf(cuboid.ext.z, 0.f,  anchor.z);
 
-	const v3 verts[] = {
-		{ xl, yl, zl }, { xl, yl, zr }, { xr, yl, zr }, { xr, yl, zl }, // Bottom
-		{ xl, yr, zl }, { xl, yr, zr }, { xr, yr, zr }, { xr, yr, zl }, // Top (rev)
+	quad faces[] = {
+		{
+			(v3) { xl, yl, zl },
+			(v3) { xl, yr, zl },
+			(v3) { xr, yr, zl },
+			(v3) { xr, yl, zl },
+		}, {
+			(v3) { xl, yr, zl },
+			(v3) { xl, yr, zr },
+			(v3) { xr, yr, zr },
+			(v3) { xr, yr, zl },
+		}, {
+			(v3) { xl, yr, zr },
+			(v3) { xl, yl, zr },
+			(v3) { xr, yl, zr },
+			(v3) { xr, yr, zr },
+		}, {
+			(v3) { xl, yl, zr },
+			(v3) { xl, yl, zl },
+			(v3) { xr, yl, zl },
+			(v3) { xr, yl, zr },
+		}, {
+			(v3) { xr, yl, zl },
+			(v3) { xr, yr, zl },
+			(v3) { xr, yr, zr },
+			(v3) { xr, yl, zr },
+		}, {
+			(v3) { xl, yl, zr },
+			(v3) { xl, yr, zr },
+			(v3) { xl, yr, zl },
+			(v3) { xl, yl, zl },
+		}
 	};
 
-	const v3 all[] = {
-		verts[0], verts[1], verts[2],
-		verts[2], verts[3], verts[0], // Bottom
-		verts[6], verts[5], verts[4],
-		verts[4], verts[7], verts[6], // Top
-		verts[5], verts[1], verts[0],
-		verts[0], verts[4], verts[5], // Left
-		verts[7], verts[3], verts[2],
-		verts[2], verts[6], verts[7], // Right
-		verts[0], verts[3], verts[7],
-		verts[7], verts[4], verts[0], // Back
-		verts[6], verts[2], verts[1],
-		verts[1], verts[5], verts[6], // Front
-	};
+	u32 i = 0;
+	for (u32 j = 0; j < sizeof(faces) / sizeof(quad); ++j) {
+		struct verts face = quad_verts(faces[j]);
+		memcpy(result.v + i, face.v, 6 * sizeof(v3));
+		free(face.v);
+		i += 6;
+	}
 
-	memcpy(result.pts, &all, sizeof(all));
 	return result;
+}
+
+static mesh cuboid_to_mesh(const cuboid cuboid)
+{
+	return verts_to_mesh(cuboid_verts(cuboid));
+}
+
+typedef struct {
+	float scale;
+	enum mesh_flags flags;
+} ico;
+
+static struct verts ico_verts(const ico ico)
+{
+	const u32 n = 20 * 3;
+	struct verts result = verts_new(n, ico.flags & MESH_TMP);
+
+	const float phi = .5f * (1.f + sqrt(5.f));
+	const float l = .5f * ico.scale;
+	const float s = .5f * ico.scale / phi;
+
+	const v3 pts[] = {
+		{  -s,  0.f,  -l }, { 0.f,   l,  -s }, {   s, 0.f,  -l }, // Front
+		{   s,  0.f,  -l }, { 0.f,  -l,  -s }, {  -s, 0.f,  -l },
+		{ 0.f,    l,  -s }, {  -l,   s, 0.f }, { 0.f,   l,   s }, // Top
+		{ 0.f,    l,   s }, {   l,   s, 0.f }, { 0.f,   l,  -s },
+		{   s,  0.f,   l }, { 0.f,   l,   s }, {  -s, 0.f,   l }, // Back
+		{  -s,  0.f,   l }, { 0.f,  -l,   s }, {   s, 0.f,   l },
+		{ 0.f,   -l,  -s }, {   l,  -s, 0.f }, { 0.f,  -l,   s }, // Bot
+		{ 0.f,   -l,   s }, {  -l,  -s, 0.f }, { 0.f,  -l,  -s },
+		{   l,    s, 0.f }, {   s, 0.f,   l }, {   l,  -s, 0.f }, // Right
+		{   l,   -s, 0.f }, {   s, 0.f,  -l }, {   l,   s, 0.f },
+		{  -l,    s, 0.f }, {  -s, 0.f,  -l }, {  -l,  -s, 0.f }, // Left
+		{  -l,   -s, 0.f }, {  -s, 0.f,   l }, {  -l,   s, 0.f },
+		{ 0.f,    l,  -s }, {   l,   s, 0.f }, {   s, 0.f,  -l }, // Corners
+		{ 0.f,    l,  -s }, {  -s, 0.f,  -l }, {  -l,   s, 0.f },
+		{ 0.f,    l,   s }, {   s, 0.f,   l }, {   l,   s, 0.f },
+		{ 0.f,    l,   s }, {  -l,   s, 0.f }, {  -s, 0.f,   l },
+		{ 0.f,   -l,  -s }, {   s, 0.f,  -l }, {   l,  -s, 0.f },
+		{ 0.f,   -l,   s }, {   l,  -s, 0.f }, {   s, 0.f,   l },
+		{ 0.f,   -l,  -s }, {  -l,  -s, 0.f }, {  -s, 0.f,  -l },
+		{ 0.f,   -l,   s }, {  -s, 0.f,   l }, {  -l,  -s, 0.f },
+	};
+
+	memcpy(result.v, pts, sizeof(pts));
+	return result;
+}
+
+static mesh ico_to_mesh(const ico ico)
+{
+	return verts_to_mesh(ico_verts(ico));
+}
+
+/* "Immediate" mesh rendering */
+
+static int mesh_wire;
+static fff mesh_col = V3_ONE;
+static fff mesh_vfx = V3_ZERO;
+static ttt mesh_trs = T3_ID;
+
+static void mesh_draw(mesh mesh)
+{
+	mesh.trs  = mesh_trs;
+	mesh.col  = mesh_col;
+	mesh.vfx  = mesh_vfx;
+	mesh.wire = mesh_wire;
+	meshes_push(&mesh);
+}
+
+static void mesh_quad(quad quad)
+{
+	quad.flags |= MESH_TMP;
+	mesh mesh = quad_to_mesh(quad);
+	mesh_draw(mesh);
+}
+
+static void mesh_bill(bill bill)
+{
+	bill.flags |= MESH_TMP;
+	mesh mesh = bill_to_mesh(bill);
+	mesh_draw(mesh);
+}
+
+static void mesh_cuboid(cuboid cuboid)
+{
+	cuboid.flags |= MESH_TMP;
+	mesh mesh = cuboid_to_mesh(cuboid);
+	mesh_draw(mesh);
+}
+
+static void mesh_ico(ico ico)
+{
+	ico.flags |= MESH_TMP;
+	mesh mesh = ico_to_mesh(ico);
+	mesh_draw(mesh);
 }
